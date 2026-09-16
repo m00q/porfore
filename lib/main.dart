15c001ada96fcd5fdc86152e2b682c3506d45d66
muq <anivisto@gmail.com>
2026-09-16 18:42:25 +0900

    폰트적용

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,18 @@ import 'package:flutter/material.dart';
 import 'l10n/app_localizations.dart';
 import 'pages/home_page.dart';
 
+String _fontFamilyFor(Locale locale) => switch (locale.languageCode) {
+  'ko' => 'KimjungchulScript',
+  'ja' => 'KleeOne',
+  _ => 'Handlee',
+};
+
+List<String> _fontFallbackFor(Locale locale) => switch (locale.languageCode) {
+  'ko' => const ['KleeOne', 'Handlee'],
+  'ja' => const ['KimjungchulScript', 'Handlee'],
+  _ => const ['KleeOne', 'KimjungchulScript'],
+};
+
 void main() => runApp(const MainApp());
 
 class MainApp extends StatefulWidget {
@@ -16,7 +28,11 @@ class _MainAppState extends State<MainApp> {
 
   @override
   Widget build(BuildContext context) => MaterialApp(
-    theme: ThemeData(scaffoldBackgroundColor: Colors.white),
+    theme: ThemeData(
+      scaffoldBackgroundColor: Colors.white,
+      fontFamily: _fontFamilyFor(_locale),
+      fontFamilyFallback: _fontFallbackFor(_locale),
+    ),
     locale: _locale,
     localizationsDelegates: AppLocalizations.localizationsDelegates,
     supportedLocales: AppLocalizations.supportedLocales,
--- lib/sections/skills_section.dart
+++ lib/sections/skills_section.dart
@@ -304,7 +304,7 @@ class _SkillsSectionState extends State<SkillsSection> {
             child: Text(
               skill.name,
               maxLines: 1,
-              style: const TextStyle(fontSize: 14),
+              style: const TextStyle(fontFamily: 'Kalam', fontSize: 18),
             ),
           ),
         ),
